89b78a702a49d4b73443fa49a5498c9f5c107503
Chad <chad.lines1@gmail.com>
2022-03-28 11:53:33 -0600

    0.030 - Log.cs complete

---
+++ Schedule Manager/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule_Manager
+{
+    class Log
+    {
+        public static void writeLog(int userId, string userName, bool success)
+        {
+            string msg = "";                                                                        // String to hold the final log message
+            string filePath = "log.txt";                                                            // The path to the log
+            string loginSuccess = $"{DateTime.Now.ToString()} - User Id:{userId.ToString()}, " +    // The successful login message
+                $"User Name: {userName}, Message: Successful Login.";
+            string loginFail = $"{DateTime.Now.ToString()} - User Id:{userId.ToString()}, " +       // The unsuccessful login message
+                $"User Name: {userName}, Message: Successful Failed.";
+
+            if (success) { msg = loginSuccess; }                                                    // Setting the appropriate message
+            else {  msg = loginFail; }
+
+            File.AppendAllText(filePath, msg);                                                      // Appending the message to the log file
+        }
+    }
+}
